b048e955aec9f3e2c348de8f47d9b57ffbe0d78b
a13140120a <71315973+a13140120a@users.noreply.github.com>
2022-04-27 17:59:14 +0800

    Update practice.sql

--- practice_with_ans/practice.sql
+++ practice_with_ans/practice.sql
@@ -1,5 +1,36 @@
-USE db01;
+ CREATE TABLE db01.food (
+		id 				char(5) 		PRIMARY KEY,
+		`name`  			varchar(30)  	NOT NULL,
+		expiredate 		datetime 		NOT NULL,
+		placeid 		char(2) 		NOT NULL,
+		price 			int unsigned 	NOT NULL,
+		catelog 		varchar(20) 	NOT NULL);
 
+CREATE TABLE `place`(
+	`id` CHAR(2) NOT NULL,
+        `name` VARCHAR(20) NULL,
+        PRIMARY KEY(`id`));
+
+INSERT INTO food VALUES ('CK001', '曲奇餅乾', '2017/01/10', 'TL', 250, '點心');
+INSERT INTO food VALUES ('CK002', '蘇打餅乾', '2020/10/12', 'TW', 80, '點心');
+INSERT INTO food VALUES ('DK001', '高山茶', '2021/05/23', 'TW', 780, '飲料');
+INSERT INTO food VALUES ('DK002', '綠茶', '2022/06/11', 'JP', 530, '飲料');
+INSERT INTO food VALUES ('OL001', '苦茶油', '2019/03/16', 'TW', 360, '調味品');
+INSERT INTO food VALUES ('OL002', '橄欖油', '2020/11/25', 'TL', 420, '調味品');
+INSERT INTO food VALUES ('CK003', '仙貝', '2020/11/01', 'JP', 270, '點心');
+INSERT INTO food VALUES ('SG001', '醬油', '2018/05/05', 'JP', 260, '調味品');
+INSERT INTO food VALUES ('OL003', '葡萄子油', '2021/05/05', 'JP', 550, '調味品');
+INSERT INTO food VALUES ('CK004', '鳳梨酥', '2020/10/12', 'TW', 340, '點心');
+INSERT INTO food VALUES ('CK005', '太陽餅', '2020/9/27', 'TW', 150, '點心');
+INSERT INTO food VALUES ('DK003', '紅茶', '2020/11/12', 'TL', 260, '飲料');
+INSERT INTO food VALUES ('SG002', '醋', '2020/09/18', 'TW', 60, '調味品');
+
+INSERT INTO place VALUES ('TW', '台灣');
+INSERT INTO place VALUES ('JP', '日本');
+INSERT INTO place VALUES ('TL', '泰國');
+INSERT INTO place VALUES ('US', '美國');
+
+-- -------------------------------------------------------------------------------------------------------
 -- 1.	查詢所有食物表格中所有欄位的資料
 SELECT * FROM food;
 -- 2.	查詢所有食物名稱、到期日和價格
