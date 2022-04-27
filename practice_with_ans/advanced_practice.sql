a3a4438d19d74fd0022562691dca887def614915
a13140120a <71315973+a13140120a@users.noreply.github.com>
2022-04-27 18:21:32 +0800

    Update advanced_practice.sql

--- practice_with_ans/advanced_practice.sql
+++ practice_with_ans/advanced_practice.sql
@@ -19,7 +19,54 @@ INSERT INTO Internet_Sales VALUES ('1999-01-11', 320);
 INSERT INTO Internet_Sales VALUES ('1999-01-12', 750);
 
 
--- 找出所有有營業額 (Sales) 的日子
+-- 1. 找出所有有營業額 (Sales) 的日子
 SELECT Txn_Date FROM Store_Information
 UNION
 SELECT Txn_Date FROM Internet_Sales;
+-- 2. 找出有店面營業額(Sales)以及網路營業額的日子
+SELECT Txn_Date FROM Store_Information
+UNION ALL
+SELECT Txn_Date FROM Internet_Sales;
+-- 3. 找出哪幾天有店面交易和網路交易
+SELECT Txn_Date FROM Store_Information
+INTERSECT
+SELECT Txn_Date FROM Internet_Sales;
+-- 4. 找出哪幾天是有店面營業額而沒有網路營業額的
+SELECT Txn_Date FROM Store_Information
+MINUS
+SELECT Txn_Date FROM Internet_Sales;
+-- 5. 找出所有在西部的店的營業額。
+SELECT SUM(Sales) FROM Store_Information
+WHERE Store_Name IN
+(SELECT Store_Name FROM Geography
+WHERE Region_Name = 'West');
+-- 或者
+SELECT SUM(a1.Sales) FROM Store_Information a1
+WHERE a1.Store_Name IN
+(SELECT Store_Name FROM Geography a2
+WHERE a2.Store_Name = a1.Store_Name);
+-- EXISTS 用法
+SELECT SUM(Sales) FROM Store_Information
+WHERE EXISTS
+(SELECT * FROM Geography
+WHERE Region_Name = 'West');
+-- 6. 顯示Store_Name跟Sales, 並將 'Los Angeles' 的 Sales 數值乘以 2，以及將 'San Diego' 的 Sales 數值乘以 1.5
+SELECT Store_Name, 
+CASE Store_Name
+  WHEN 'Los Angeles' THEN Sales * 2
+  WHEN 'San Diego' THEN Sales * 1.5
+  ELSE Sales
+  END
+"New Sales",
+Txn_Date
+FROM Store_Information;
+
+
+
+
+
+
+
+
+
+
