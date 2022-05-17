6604b8e5f370fd5f9e7ef421cab2553ba2f7c71d
a13140120a <71315973+a13140120a@users.noreply.github.com>
2022-05-17 11:22:58 +0800

    Update advanced_practice.sql

--- practice_with_ans/advanced_practice.sql
+++ practice_with_ans/advanced_practice.sql
@@ -117,14 +117,14 @@ WHERE Region_Name = 'West');
 SELECT SUM(a1.Sales) FROM Store_Information a1
 WHERE a1.Store_Name IN
 (SELECT Store_Name FROM Geography a2
-WHERE a2.Store_Name = a1.Store_Name);
+WHERE a2.Store_Name = a1.Store_Name AND a2.Region_Name='West');
 -- EXISTS 用法
 SELECT 
     SUM(Sales) 
 FROM 
     Store_Information s 
 WHERE 
-    EXISTS (SELECT Store_Name FROM Geography g WHERE s.Store_Name=g.Store_Name AND  g.Region_Name = 'West');
+    EXISTS (SELECT 1 FROM Geography g WHERE s.Store_Name=g.Store_Name AND  g.Region_Name = 'West');
 -- 6. 顯示Store_Name跟Sales, 並將 'Los Angeles' 的 Sales 數值乘以 2，以及將 'San Diego' 的 Sales 數值乘以 1.5
 SELECT Store_Name, 
 CASE Store_Name
